a468255260c32d375d2a0be533b5efcf3a7bd7f5
JStroo2000 <jelmers@live.com>
2026-03-24 16:36:51 +0100

    Train.py passes linting

--- src/model/train.py
+++ src/model/train.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import train_test_split
 
 import mlflow
 
+
 # define functions
 def main(args):
     # TO DO: enable autologging
@@ -37,9 +38,13 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    X, y = df[['Pregnancies', 'PlasmaGlucose',
+               'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin',
+               'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=0.30, random_state=0)
     return X_train, X_test, y_train, y_test
+
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
@@ -61,6 +66,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
